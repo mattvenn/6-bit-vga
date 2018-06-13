52790e456c2743cc952c4597f0357b93620ccc2f
matt venn <matt@mattvenn.net>
2018-06-13 14:22:37 +0200

    modified for the new boards

--- top.v
+++ top.v
@@ -5,10 +5,12 @@ module top (
     output LED,
     output hsync,
     output vsync,
-    output g0,
+    output r1,
+    output r2,
     output g1,
     output g2,
-    output g3
+    output b1,
+    output b2
 
 );
 
@@ -19,9 +21,11 @@ module top (
 
     VgaSyncGen vga_inst( .clk(clk), .hsync(hsync), .vsync(vsync), .x_px(x_px), .y_px(y_px), .px_clk(px_clk), .activevideo(activevideo));
 
-    assign g0 = x_px > 200 ? 1 : 0;
-    assign g1 = x_px > 400 ? 1 : 0;
-    assign g2 = y_px > 150 ? 1 : 0;
-    assign g3 = y_px > 300 ? 1 : 0;
+    assign g1 = x_px > 200 ? 1 : 0;
+    assign g2 = x_px > 400 ? 1 : 0;
+    assign r1 = y_px > 150 ? 1 : 0;
+    assign r2 = y_px > 300 ? 1 : 0;
+    assign b1 = x_px > 300 ? 1 : 0;
+    assign b2 = y_px > 250 ? 1 : 0;
 
 endmodule
